0896d05cb6747f4c24c3bd8ac01992f34190ff39
Aneece Banoun <32448666+aneecebanoun@users.noreply.github.com>
2023-01-19 08:36:53 +0000

    Update TicketServiceTests.java

--- src/test/java/aneece/banoun/dwp/TicketServiceTests.java
+++ src/test/java/aneece/banoun/dwp/TicketServiceTests.java
@@ -9,7 +9,6 @@ import aneece.banoun.dwp.uk.gov.dwp.uc.pairtest.TicketService;
 import aneece.banoun.dwp.uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import aneece.banoun.dwp.uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import aneece.banoun.dwp.uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
-import com.jayway.jsonpath.internal.Utils;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,15 +63,16 @@ class TicketServiceTests {
         groupTicketsMethod.setAccessible(true);
         List<TicketTypeRequest> validData = ticketServiceTestData.get(VALID_BOOKING_ID);
         Map<TicketTypeRequest.Type, List<TicketTypeRequest>> groupTickets = (Map<TicketTypeRequest.Type, List<TicketTypeRequest>>) groupTicketsMethod.invoke(ticketService, validData);
-        Method getTotalAmountToPayMethod = TicketServiceImpl.class.getDeclaredMethod("getTotalAmountToPay", Map.class);
-        getTotalAmountToPayMethod.setAccessible(true);
-        getTotalAmountToPayMethod.invoke(ticketService, groupTickets);
-        Integer totalAmountToPay = (Integer) getTotalAmountToPayMethod.invoke(ticketService, groupTickets);
-        Method getTotalSeatsToAllocateMethod = TicketServiceImpl.class.getDeclaredMethod("getTotalSeatsToAllocate", Map.class);
-        getTotalSeatsToAllocateMethod.setAccessible(true);
-        Integer totalSeatsToAllocate = (Integer) getTotalSeatsToAllocateMethod.invoke(ticketService, groupTickets);
+        Integer totalAmountToPay = getCounts(groupTickets, "getTotalAmountToPay");
+        Integer totalSeatsToAllocate = getCounts(groupTickets, "getTotalSeatsToAllocate");
         assertEquals(8, totalSeatsToAllocate);
         assertEquals(130, totalAmountToPay);
+    }
+
+    private Integer getCounts(Map<TicketTypeRequest.Type, List<TicketTypeRequest>> groupTickets, String methodName) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method method = TicketServiceImpl.class.getDeclaredMethod(methodName, Map.class);
+        method.setAccessible(true);
+        return (Integer) method.invoke(ticketService, groupTickets);
     }
 
 }
